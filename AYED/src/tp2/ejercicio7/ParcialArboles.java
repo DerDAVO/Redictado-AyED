adf0ebdfdf58e31b7452d5479dcc5a61d0915a3c
DerDAVO <marcos.matias180834@alumnos.info.unlp.edu.ar>
2024-10-21 15:23:19 -0300

    tp2/ejercicio7

---
+++ AYED/src/tp2/ejercicio7/ParcialArboles.java
@@ -0,0 +1,45 @@
+package tp2.ejercicio7;
+import tp2.ejercicio1y2.BinaryTree;
+public class ParcialArboles {
+
+    private BinaryTree<Integer> ab;
+    public BinaryTree<Integer> buscarNodo(int num,BinaryTree<Integer> ab){
+        //Buscar un  nodo cuyo valor sea num
+        if(ab.getData() == num){
+            return ab;
+        }
+        BinaryTree<Integer> aux = null;
+        if(ab.hasLeftChild()){
+            aux = buscarNodo(num,ab.getLeftChild());
+        }
+        if(ab.hasRightChild() && aux==null){
+            aux = buscarNodo(num,ab.getRightChild());
+        }
+        return aux;
+    }
+
+    // Este metodo tiene el fin de verificar si el hijo izquierdo del nodo con valr num
+    // tiene mas hijos con un solo nodo que el del hijo derecho
+    public int unicoHijo(BinaryTree<Integer> ab){
+        int aux = 0;
+        if(ab.hasLeftChild()){
+            aux += unicoHijo(ab.getLeftChild());
+        } 
+        if(ab.hasRightChild()){
+            aux += unicoHijo(ab.getRightChild());
+        }
+        if((ab.hasLeftChild() && !ab.hasRightChild())||(ab.hasRightChild()&&!ab.hasLeftChild())){
+            aux++;
+        }
+        return aux;
+    }
+    public boolean isLeftTree(int num){
+        //Buscar un  nodo cuyo valor sea num
+        BinaryTree bt = buscarNodo(num);
+        int hi = -1,hd = -1;
+        hi = unicoHijo(bt.getLeftChild());
+        hd = unicoHijo(bt.getRightChild());
+        return hi>hd;
+    }
+
+}
