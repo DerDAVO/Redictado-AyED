ca017e211fec584c69581c5e508aa2bc1340557c
DerDAVO <marcos.matias180834@alumnos.info.unlp.edu.ar>
2024-11-04 11:20:34 -0300

    carpeta P3

---
+++ AYED/src/tp3/ejercicio1/MainPrueba.java
@@ -0,0 +1,20 @@
+package tp3.ejercicio1;
+
+public class MainPrueba {
+    public static void recorridoPreOrden(){
+        if(){
+            
+        }
+
+
+    }
+    
+    public static void main(String[] args) {
+        GeneralTree<Integer> tree = new GeneralTree<Integer>(1);
+        tree.addChild(new GeneralTree<Integer>(2));
+        tree.addChild(new GeneralTree<Integer>(3));
+        tree.addChild(new GeneralTree<Integer>(4));
+        tree.addChild(new GeneralTree<Integer>(5));
+        
+    }
+}
